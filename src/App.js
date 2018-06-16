e192683308fcd449855abc724abe7eeb52227189
kinny94 <ad.sam007@gmail.com>
2018-06-16 18:49:16 -0400

    Implmented working routes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './header/header';
 
-import Main from './components/main';
-import MainArray from './components/array/main';
-import MainStack from './components/stack/main';
+import Main from './components/Main';
+import MainArray from './components/array/MainArray';
+import MainStack from './components/stack/MainStack';
 import MainQueue from './components/queue/main';
-import MainTrees from './components/trees/main';
-import MainGraphs from './components/graph/main';
-import MinLinkedList from './components/linkedlist/main';
-import MainSearching from './components/searching/main';
+import MainTrees from './components/trees/MainTrees';
+import MainGraphs from './components/graph/MainGraph';
+import MainLinkedList from './components/linkedlist/MainLinkedList';
+import MainSearching from './components/searching/MainSearching';
 import MainSorting from './components/sorting/main';
-import MainLinkedList from './components/linkedlist/main';
 
 class App extends Component {
 	
@@ -23,17 +22,19 @@ class App extends Component {
 		return (
 			<div className="App">
 				<BrowserRouter>
-					<Header />
-					<div className="container">
-						<Route path="/" exact component={ Main }/>
-						<Route path="/array" exact component={ MainArray } />
-						<Route path="/stack" exact component={ MainStack } />
-						<Route path="/queue" exact  component={ MainQueue } />
-						<Route path="/linkedlist" exact component={ MainLinkedList } />
-						<Route path="/trees" exact component={ MainTrees} />
-						<Route path="/graphs" exact component={ MainGraphs } />
-						<Route path="/sorting" exact component={ MainSorting } />
-						<Route path="/searching" exact component={ MainSearching } />
+					<div>
+						<Header />
+						<div className="container">
+							<Route path="/" exact component={ Main }/>
+							<Route path="/array" exact component={ MainArray } />
+							<Route path="/stack" exact component={ MainStack } />
+							<Route path="/queue" exact  component={ MainQueue } />
+							<Route path="/linkedlist" exact component={ MainLinkedList } />
+							<Route path="/trees" exact component={ MainTrees} />
+							<Route path="/graphs" exact component={ MainGraphs } />
+							<Route path="/sorting" exact component={ MainSorting } />
+							<Route path="/searching" exact component={ MainSearching } />
+						</div>
 					</div>
 				</BrowserRouter>
 			</div>
---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This would be the landing componen</h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
