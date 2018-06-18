3758f741e5504c397983ee800b746503d89fa2b6
kinny94 <ad.sam007@gmail.com>
2018-06-17 20:29:12 -0400

    created the front page

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Main extends Component{
 
@@ -18,30 +19,39 @@ class Main extends Component{
             <div className="cards-margin">
                 <div className="row"> 
                     <div className="col-4">
-                        <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Arrays</h2>
-                                <p className="card-text bottom">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <Link to={ '/array' } >
+                            <div className="card-banner  overlay-grad" style={ style }>
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Arrays</h2>
+                                    <p className="card-text bottom">An array data structure, or simply an array, is a data structure consisting of a collection of elements (values or variables), each identified by at least one array index or key.</p>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     </div>
+                   
 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Stacks</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/stacks' }>
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Stacks</h2>
+                                    <p className="card-text">A stack is an abstract data type that serves as a collection of elements, with two principal operations:
+                                        push, which adds an element to the collection, and pop, which removes the most recently added element that was not yet remove.
+                                    </p>
+                                </div>
+                            </Link>
                         </div>
                     </div>
 
 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h5 className="card-title">Queue</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/queue' }>
+                                <div className="card-body text-white">
+                                    <h5 className="card-title">Queue</h5>
+                                    <p className="card-text">a queue is a particular kind of abstract data type or collection in which the entities in the collection are kept in order and the principal operations on the collection are the addition of entities to the rear terminal position, known as enqueue, and removal of entities from the front terminal position, known as dequeue.</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>    
                 </div>
@@ -49,63 +59,74 @@ class Main extends Component{
                 <div className="row cards-margin"> 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Linked List</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/linkedlist' } >
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Linked List</h2>
+                                    <p className="card-text">A linked list is a linear collection of data elements, whose order is not given by their physical placement in memory. Instead, each element points to the next. It is a data structure consisting of a collection of nodes which together represent a sequence.</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>
 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Trees</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/trees' } >
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Tree</h2>
+                                    <p>A tree is a widely used abstract data type (ADT)—or data structure implementing this ADT—that simulates a hierarchical tree structure, with a root value and subtrees of children with a parent node, represented as a set of linked nodes.</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>
 
 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Graphs</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/graphs' }>
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Graphs</h2>
+                                    <p className="card-text">A graph is an abstract data type that is meant to implement the undirected graph and directed graph concepts from mathematics, specifically the field of graph theory.</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>    
                 </div>
 
                 <div className="row cards-margin"> 
                     <div className="col-4">
-                        <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Searching</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <Link to={ '/searching'} >
+                            <div className="card-banner  overlay-grad" style={ style }>
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Searching</h2>
+                                    <p className="card-text">A search algorithm is any algorithm which solves the search problem, namely, to retrieve information stored within some data structure, or calculated in the search space of a problem domain.</p>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
                     </div>
 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Sorting</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/sorting' }>
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Sorting</h2>
+                                    <p className="card-text">A sorting algorithm is an algorithm that puts elements of a list in a certain order. The most frequently used orders are numerical order and lexicographical order. Efficient sorting is important for optimizing the efficiency of other algorithms which require input data to be in sorted list.</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>
 
 
                     <div className="col-4">
                         <div className="card-banner  overlay-grad" style={ style }>
-                            <div className="card-body text-white">
-                                <h2 className="card-title">Miscellaneous</h2>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <Link to={ '/miscellaneous' } >
+                                <div className="card-body text-white">
+                                    <h2 className="card-title">Miscellaneous</h2>
+                                    <p className="card-text">Contains a number of miscellaneous questions that are usually asked during an ibnterview. This link may contain quewtions qhich uses more than one data structure or some complex problems.</p>
+                                </div>
+                            </Link>
                         </div>
                     </div>    
                 </div>
-
             </div>
         )
     }
