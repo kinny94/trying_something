fa8918fac416e64519c093776f9d54e076934f45
kinny94 <ad.sam007@gmail.com>
2018-06-25 18:20:35 -0400

    Implemented tree content

--- client/src/components/trees/MainTrees.js
+++ client/src/components/trees/MainTrees.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class MainTrees extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            problems: []
+        }
+    }
+    
+    componentDidMount(){  
+        fetch( '/tree' ).then(( data ) => {
+            return data.json()
+        }).then(( msg ) => {
+
+            let allProblems = [];
+            for( let i=0; i<msg.data.length; i++ ){
+                let problemName = msg.data[i].replace( ".java", "" ).replace(/_/g, " ");
+                allProblems.push( problemName );
+            }
+
+            this.setState({
+                problems: allProblems
+            });
+        });
+    }
+    
+    
+    renderProblems = () => {
+
+        if( this.state.problems.length > 0 ){
+            return this.state.problems.map(( problem ) => {
+                return <Link key={ problem }  to={ `/problem/trees/${ problem }` } ><li className="list-group-item">{ problem }</li></Link>
+            });
+        }else{
+            return(
+                <div className="center-text">
+                    <h2>Loading Problems...</h2>
+                </div>
+            )
+        }
+    }
+
     render(){
         return(
             <div className="container margin">
@@ -8,10 +50,18 @@ class MainTrees extends Component{
                     <h2 className="text-center">Array</h2>
                 </div>
                 <div className="row">
-                    <p className="text-left">An array data structure, or simply an array, is a data structure consisting of a collection of elements 
-                       (values or variables), each identified by at least one array index or key. An array is stored such that the 
-                        position of each element can be computed from its index tuple by a mathematical formula. The simplest type 
-                        of data structure is a linear array, also called one-dimensional array.
+                    <p className="text-left">
+                        A tree is a widely used abstract data type (ADT)—or data structure implementing this ADT—that 
+                        simulates a hierarchical tree structure, with a root value and subtrees of children with a parent 
+                        node, represented as a set of linked nodes. A tree data structure can be defined recursively 
+                        (locally) as a collection of nodes (starting at a root node), where each node is a data structure 
+                        consisting of a value, together with a list of references to nodes (the "children"), with the 
+                        constraints that no reference is duplicated, and none points to the root.
+                        Alternatively, a tree can be defined abstractly as a whole (globally) as an ordered tree, with a 
+                        value assigned to each node. Both these perspectives are useful: while a tree can be analyzed 
+                        mathematically as a whole, when actually represented as a data structure it is usually represented 
+                        and worked with separately by node (rather than as a set of nodes and an adjacency list of edges 
+                         between nodes, as one may represent a digraph, for instance). 
                     </p>
                 </div>
                 <div className="row margin">
@@ -23,41 +73,95 @@ class MainTrees extends Component{
                         <thead>
                             <tr>
                                 <th scope="col">Type</th>
-                                <th scope="col">Complexity</th>
+                                <th scope="col">Binary Search Tree</th>
+                                <th scope="col">Cartesian Tree</th>
+                                <th scope="col">Binary Tree</th>
+                                <th scope="col">Red Black Tree</th>
+                                <th scope="col">Splay Tree</th>
+                                <th scope="col">AVL Tree</th>
+                                <th scope="col">KD Tree</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr className="table-active">
                                 <th scope="row">Access ( Average )</th>
-                                <td>Θ(1)</td>
+                                <td>O(log(n))</td>
+                                <td>N/A</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>N/A</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
                             </tr>
-                            <tr>
+                            <tr className="table-active">
                                 <th scope="row">Search ( Average )</th>
-                                <td>Θ(n)</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
                             </tr>
-                            <tr className="table-primary">
+                            <tr className="table-active">
                                 <th scope="row">Insertion ( Average )</th>
-                                <td>Θ(n)</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
                             </tr>
-                            <tr>
+                            <tr className="table-active">
                                 <th scope="row">Deletion ( Average )</th>
-                                <td>Θ(n)</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
                             </tr>
-                            <tr className="table-primary">
+                            <tr className="table-active">
                                 <th scope="row">Access ( Worst )</th>
-                                <td>Θ(1)</td>
+                                <td>O(n)</td>
+                                <td>N/A</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>N/A</td>
+                                <td>O(log(n))</td>
+                                <td>O(n)</td>
                             </tr>
-                            <tr>
+                            <tr className="table-active">
                                 <th scope="row">Search ( Worst )</th>
-                                <td>Θ(n)</td>
+                                <td>O(n)</td>
+                                <td>O(n)</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(n)</td>
                             </tr>
-                            <tr className="table-primary">
+                            <tr className="table-active">
                                 <th scope="row">Insertion ( Worst )</th>
-                                <td>Θ(n)</td>
+                                <td>O(n)</td>
+                                <td>O(n)</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(n)</td>
                             </tr>
-                            <tr>
+                            <tr className="table-active">
                                 <th scope="row">Deletion ( Worst )</th>
-                                <td>Θ(n)</td>
+                                <td>O(n)</td>
+                                <td>O(n)</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(log(n))</td>
+                                <td>O(n)</td>
                             </tr>
                         </tbody>
                     </table> 
