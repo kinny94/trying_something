b2be3172619de59de8c24990227fbe28d0e31075
kinny94 <ad.sam007@gmail.com>
2019-02-04 21:39:37 -0500

    feat: read all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'codebase';
+  data = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('assets/text.txt', {responseType: `text`}).subscribe(data => this.data = data);
+  }
 }
