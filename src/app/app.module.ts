106e51938e36504104d128086a74b92274b0f3c1
kinny94 <ad.sam007@gmail.com>
2019-02-09 13:40:13 -0500

    feat(component): add array component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
-import {FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-import {MaterialModule} from './material.module';
+import { MaterialModule } from './material.module';
 import { HeaderComponent } from './header/header.component';
 import { GradientCardComponent } from './components/gradient-card/gradient-card.component';
 import { HomeComponent } from './components/home/home.component';
+import { ArrayComponent } from './components/data-structures/array/array.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GradientCardComponent,
-    HomeComponent
+    HomeComponent,
+    ArrayComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +27,8 @@ import { HomeComponent } from './components/home/home.component';
     MaterialModule,
     FlexLayoutModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent }
+      { path: '', component: HomeComponent },
+      { path: 'array', component: ArrayComponent },
     ])
   ],
   providers: [],
