a7b9ed50459c851269941d6f4a9a33d2c19ae132
kinny94 <ad.sam007@gmail.com>
2019-02-09 19:23:24 -0500

    refactor(structure): move problems to the root directory

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist/codebase'));
 
 const getFilesFromFolder = (path) => {
-    return fs.readdirSync('./src/assets/programming/array/', (err, files) => {
+    return fs.readdirSync(path, (err, files) => {
         return files;
     });
 } 
 
 app.use(homeRoutes);
 app.get('/api/cards', (req, res) => {
-    const folders = getDirectories('./src/assets/programming/');
+    const folders = getDirectories('./problems/');
     res.send({data: folders})
 })
 
 app.get('/api/:topic', (req, res) => {
-    const files = getFilesFromFolder(`./src/assets/programming/${req.params.topic}/`)
+    const files = getFilesFromFolder(`./problems/${req.params.topic}/`)
     res.send({ data: files});
 });
 
