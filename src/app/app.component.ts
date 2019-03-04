5448adb29518b5be2c2b9c6cc1187f4ca2f1d623
kinny94 <ad.sam007@gmail.com>
2019-03-04 14:07:56 -0500

    feat(firebase): add firebase to te project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,11 @@ export class AppComponent implements OnInit {
   title = 'codebase';
   data = '';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private storage: AngularFireStorage) {
   }
 
   ngOnInit() {
+    const ref = this.storage.ref('array.txt');
+    ref.getDownloadURL().subscribe(data =>  console.log(data));
   }
 }
