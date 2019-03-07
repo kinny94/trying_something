078ad22d50f73084aaad6dafd11f9fbbafa75053
kinny94 <ad.sam007@gmail.com>
2019-03-07 16:24:16 -0500

    feat(Upload-Form): add mat design to the form

--- src/app/components/upload-form/upload-form.component.ts
+++ src/app/components/upload-form/upload-form.component.ts
@@ -1,7 +1,21 @@
+import { filter, take } from 'rxjs/operators';
+import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
-
+const TAGS = [
+  'Array',
+  'LinkedList',
+  'Stack',
+  'Queue',
+  'Graph',
+  'Tree',
+  'Algorithms',
+  'Searching',
+  'Sorting',
+  'Dynamic Programming',
+  'Hash-Table'
+];
 @Component({
   selector: 'app-upload-form',
   templateUrl: './upload-form.component.html',
@@ -17,9 +31,28 @@ export class UploadFormComponent implements OnInit {
     complexity: new FormControl('')
   });
 
+  tagSelection ?: Observable<Array<string>>;
+  selectedTagsSubject: BehaviorSubject<Array<string>> = new BehaviorSubject<Array<string>>([]);
+  selectedTags: Observable<Array<string>> = this.selectedTagsSubject.asObservable();
+
   constructor() { }
 
   ngOnInit() {
+    this.tagSelection = of(TAGS);
+  }
+
+  onTagSelected(value) {
+
+    this.selectedTags.pipe(take(1)).subscribe(tags => {
+      const newArr = [...tags, value];
+      this.selectedTagsSubject.next(newArr);
+    });
+
+    this.tagSelection = this.tagSelection.pipe(
+      filter(tag => tag !== value)
+    );
+
+    this.selectedTags.subscribe(data => console.log(data));
   }
 
   onSubmit() {
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyCzV7f7wtbuxeIZYT3fjm4r0dSbzLLU-3U',
+		authDomain: 'codebase-e1c81.firebaseapp.com',
+		databaseURL: 'https://codebase-e1c81.firebaseio.com',
+		projectId: 'codebase-e1c81',
+		storageBucket: 'codebase-e1c81.appspot.com',
+		messagingSenderId: '140641604588'
+	}
+};
