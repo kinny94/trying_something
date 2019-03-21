f952bd623ec835b97fc724a0d0ba0ed15f0421a8
kinny <ad.sam007@gmail.com>
2019-03-21 19:34:52 -0400

    test(testing): add test suite for signup component

---
+++ src/app/components/signup/signup.component.spec.ts
@@ -0,0 +1,46 @@
+import { SignupComponent } from './signup.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from './../../material.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
+
+describe('SignUpcomponent', () => {
+
+  let comp: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        SignupComponent
+      ],
+      imports: [
+        MaterialModule,
+        AngularFireModule,
+        AngularFireDatabaseModule,
+        AngularFireStorageModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([
+          { path: 'signup', component: SignupComponent, pathMatch: 'full'}
+        ])
+      ],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
+    });
+
+    fixture = TestBed.createComponent(SignupComponent);
+    comp = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the signup component', () => {
+    expect(comp).toBeTruthy();
+  });
+});
