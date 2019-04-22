30787fe32fbbe5e9dd40d6b3de8abe7a81ea74c8
kinny <ad.sam007@gmail.com>
2019-04-21 20:51:57 -0400

    feat(profile-page): add form for updating profile and password change

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user-service/user.service';
-import { Observable, of, BehaviorSubject } from 'rxjs';
-import { UserData, ProblemData } from 'src/models/model';
-import { map, flatMap, switchMap, mergeMap } from 'rxjs/operators';
-import { ProblemsService } from 'src/app/services/problems/problems.service';
+import { Observable } from 'rxjs';
+import { UserData, Username } from 'src/models/model';
+import { map } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { checkPasswords } from '../signup/validators';
+import { ErrorStateMatcher } from '@angular/material';
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 
 @Component({
   selector: 'app-user',
@@ -16,10 +28,33 @@ export class UserComponent implements OnInit {
   likedProblems$: Observable<Object>;
   ratedProblems$: Observable<Object>;
 
+  updateProfileForm: FormGroup;
+  changePassowordForm: FormGroup;
+
+  hidePassword = true;
+  hideConfirmPassword = true;
+
+  showChangePasswordForm = false;
+
   constructor(
     private userService: UserService,
-    private problemService: ProblemsService,
-  ) { }
+    private formBuilder: FormBuilder,
+  ) {
+    this.updateProfileForm = this.formBuilder.group({
+      firstname: ['', [ Validators.required ]],
+      lastname: [''],
+      username: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern(/^[a-z0-9]+$/i)
+      ]],
+    });
+
+    this.changePassowordForm = this.formBuilder.group({
+      password: ['', [Validators.minLength(8), Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    }, {validator: checkPasswords });
+  }
 
   ngOnInit() {
     this.userdata$ = this.userService.getUserData();
@@ -33,6 +68,36 @@ export class UserComponent implements OnInit {
         return userdata.ratedProblems;
       }),
     );
+  }
+
+  usernameTaken(username: string): Observable<boolean> {
+    return this.userService.isUsernameTaken(username).pipe(
+      map((user: Username) => {
+        if (user === null) {
+          return false;
+        } else {
+          return true;
+        }
+      }),
+    );
+  }
+
+  changeForm() {
+    this.showChangePasswordForm = !this.showChangePasswordForm;
+  }
+
+  onSubmit() {
+    this.showChangePasswordForm ? this.changePassword() : this.updateProfile();
+  }
+
+  updateProfile() {
+    if (this.updateProfileForm.valid) {
+
+    }
+  }
+
+  changePassword() {
+    console.log('I am gonna change password');
   }
 
 }
