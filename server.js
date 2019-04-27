5a94b5e23a6f66a2147ded0fbb0b37b72a6175eb
kinny <ad.sam007@gmail.com>
2019-04-27 07:14:03 -0400

    feat(node-server): add node server and fix bugs

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const request = require('request');
+
+const app = express();
+const router = express.Router();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist/codebase'));
+
+router.get('/api', (req, res) => {
+  console.log('something');   
+  res.send({content});
+}); 
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname));
+});
+
+app.listen(port, () => {
+    console.log('server running on port : ' + port);
+});
